e71bd1811066feb45ef9faeeaa7bb5b59d4175d5
kill-ux <mustaphaboutoubdev@gmail.com>
2026-03-31 10:16:00 +0100

    install dart

--- data-structures.dart
+++ data-structures.dart
@@ -1,13 +1,6 @@
-
 var listNum = [0, 0, 0, 0, 0];
 var listObj = [0, 3.14, "str", true];
-var listStr = ["list","of","strings"];
+var listStr = ["list", "of", "strings"];
 var listList = [listNum, listObj, listStr];
 var setStr = {'Japan', 'Tokyo', 'Shiboya'};
-var mapStr = {
-	2025: "last",
-	2026: "current",
-	2027: 'next'
-}; 
-
-
+var mapStr = {"last": 2025, "current": 2026, 'next': 2027};
