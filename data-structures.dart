f168ab26e308f7fe50689a510726bb61a910dc14
kill-ux <mustaphaboutoubdev@gmail.com>
2026-03-31 09:54:11 +0100

    setStr

--- data-structures.dart
+++ data-structures.dart
@@ -3,7 +3,7 @@ var listNum = [0, 0, 0, 0, 0];
 var listObj = [0, 3.14, "str", true];
 var listStr = ["list","of","strings"];
 var listList = [listNum, listObj, listStr];
-var set = {'Japan', 'Tokyo', 'Shiboya'};
+var setStr = {'Japan', 'Tokyo', 'Shiboya'};
 var mapStr = {
 	2025: "last",
 	2026: "current",
